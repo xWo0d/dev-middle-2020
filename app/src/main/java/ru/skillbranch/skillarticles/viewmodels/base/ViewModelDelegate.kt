38fbd17d4337955830d88e613108fcfb48f82511
Victor <vdor72@gmail.com>
2020-06-29 01:37:31 +0300

    fixed ViewModelDelegate in case arg is null

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/ViewModelDelegate.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/ViewModelDelegate.kt
@@ -12,6 +12,8 @@ class ViewModelDelegate<T : ViewModel>(
 ): ReadOnlyProperty<FragmentActivity, T> {
 
     override fun getValue(thisRef: FragmentActivity, property: KProperty<*>): T {
-        return ViewModelProviders.of(thisRef, ViewModelFactory(arg)).get(clazz)
+        return arg?.let {
+            ViewModelProviders.of(thisRef, ViewModelFactory(arg)).get(clazz)
+        } ?: ViewModelProviders.of(thisRef).get(clazz)
     }
 }
