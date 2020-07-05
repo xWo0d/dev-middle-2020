78dbdc992abb26eb4333708e09086e38d3c8d602
Victor <vdor72@gmail.com>
2020-07-05 14:44:02 +0300

    refactored Notify class:   add abstract field message   override message for Notify child

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/BaseViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/BaseViewModel.kt
@@ -126,20 +126,21 @@ class EventObserver<E>(private val onEventUnhandledContent: (E) -> Unit) : Obser
     }
 }
 
-sealed class Notify(val message: String) {
+sealed class Notify {
 
-    data class TextMessage(val msg: String) : Notify(msg)
+    abstract val message: String
+    data class TextMessage(override val message: String) : Notify()
 
     data class ActionMessage(
-        val msg: String,
+        override val message: String,
         val actionLabel: String,
         val actionHandler: (() -> Unit)
-    ) : Notify(msg)
+    ) : Notify()
 
     data class ErrorMessage(
-        val msg: String,
+        override val message: String,
         val errorLabel: String?,
         val errHandler: (() -> Unit)?
-    ) : Notify(msg)
+    ) : Notify()
 
 }
