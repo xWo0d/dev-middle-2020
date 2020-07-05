2d3725876e5d540afaea14d549d8fd0079c8f157
Victor <vdor72@gmail.com>
2020-07-05 14:23:59 +0300

    refactored handleSearch to simplify code fixed out of bounds exception in case when we stay on some non zero search position and modify search query

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
@@ -103,12 +103,11 @@ class ArticleViewModel(private val articleId: String):
     }
 
     override fun handleSearch(query: String?) {
-        val result = query
-            .takeIf { !it.isNullOrEmpty() }
-            ?.let { (currentState.content.firstOrNull() as? String).indexesOf(it)
-            .map { index -> index to index + it.length } }
-            ?: emptyList()
-        updateState { it.copy(searchQuery = query, searchResults = result) }
+        query ?: return
+        val result = (currentState.content.firstOrNull() as? String)
+            .indexesOf(query)
+            .map { index -> index to index + query.length }
+        updateState { it.copy(searchQuery = query, searchResults = result, searchPosition = 0) }
     }
 
     override fun handleLike() {
