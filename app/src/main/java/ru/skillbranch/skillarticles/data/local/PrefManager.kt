3c16e2d726b2c366e53dd1e260bf49b8212acf44
Victor <vdor72@gmail.com>
2020-07-05 15:15:43 +0300

    implemented PrefDelegate and PrefManager

---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -0,0 +1,22 @@
+package ru.skillbranch.skillarticles.data.local
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+import ru.skillbranch.skillarticles.data.delegates.PrefDelegate
+
+class PrefManager(context: Context) {
+
+    internal val preferences: SharedPreferences by lazy {
+        PreferenceManager.getDefaultSharedPreferences(context)
+    }
+
+    var storedBoolean by PrefDelegate(false)
+    var storedString by PrefDelegate("test")
+    var storedInt by PrefDelegate(Int.MAX_VALUE)
+    var storedLong by PrefDelegate(Long.MAX_VALUE)
+    var storedFloat by PrefDelegate(100f)
+
+    fun clearAll() = preferences.edit().clear().apply()
+
+}
