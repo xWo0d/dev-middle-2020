bcd61bcf2193a608a621c3116590beb9704d568c
Victor <vdor72@gmail.com>
2020-10-18 18:22:41 +0300

    fixed tv_alt top padding

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/markdown/MarkdownImageView.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/markdown/MarkdownImageView.kt
@@ -13,6 +13,7 @@ import androidx.annotation.VisibleForTesting
 import androidx.core.animation.doOnEnd
 import androidx.core.graphics.ColorUtils
 import androidx.core.view.isVisible
+import androidx.core.view.setPadding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
@@ -125,7 +126,7 @@ class MarkdownImageView private constructor(
                 setBackgroundColor(ColorUtils.setAlphaComponent(colorSurface, 160))
                 gravity = Gravity.CENTER
                 textSize = fontSize
-                setPaddingOptionally(top = titleTopMargin)
+                setPadding(titleTopMargin)
                 isVisible = false
             }
         }
